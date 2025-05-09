f6d14e14558b5c1ddd9d8d52aa3743232da4a7e0
oayoade <olusholaayoade@gmail.com>
2025-05-09 16:01:18 +0200

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Autobud!</h1>
+        <Signup />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Signup() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/signup', {
+        username,
+        email,
+        password
+      });
+      alert('User created with ID: ' + response.data.id);
+    } catch (error) {
+      alert('Error creating user: ' + error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default Signup;

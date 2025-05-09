99866aef1c0f65b89c60a65db2b4ad57408c8d09
oayoade <olusholaayoade@gmail.com>
2025-05-09 16:48:41 +0200

    req

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database URL: Update these values to match your PostgreSQL configuration
-DATABASE_URL = "postgresql://user:password@db:5432/autobud_db"
+DATABASE_URL = "postgresql://admin:olushola@db:5432/main_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
